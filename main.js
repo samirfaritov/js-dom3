a31ad90ac81597a9d09404d117b52217914d8a27
samirfaritov <samirfaritov6@gmail.com>
2023-11-03 20:40:11 +0500

    xatto to'g'ri shud

--- main.js
+++ main.js
@@ -5,20 +5,20 @@ let add = document.querySelector('#add')
 let item = document.querySelector('.item')
 let br = document.querySelector('#br')
 let position = document.querySelector('#position')
-let right = document.querySelector('#right')
 let left = document.querySelector('#left')
-let bottom = document.querySelector('#bottom')
 // let top = document.querySelector('#top')
+let right = document.querySelector('#right')
+let bottom = document.querySelector('#bottom')
 
  let w = 0
  let h = 0
  let bg = ``
  let b = 0
  let pos = ``
- let r = 0
  let l = 0
- let bot = 0
  let t = 0
+ let r = 0
+ let bot = 0
 
 width.addEventListener('keyup' , (event) => {
      w = event.target.value
@@ -39,20 +39,20 @@ position.addEventListener('keyup' , (event) => {
      pos = event.target.value
     
 })
-right.addEventListener('keyup' , (event) => {
-     r = event.target.value
-    
-})
 left.addEventListener('keyup' , (event) => {
      l = event.target.value
     
 })
-bottom.addEventListener('keyup' , (event) => {
-     bot = event.target.value
-    
-})
 top.addEventListener('keyup' , (event) => {
      t = event.target.value
+    
+})
+right.addEventListener('keyup' , (event) => {
+     r = event.target.value
+    
+})
+bottom.addEventListener('keyup' , (event) => {
+     bot = event.target.value
     
 })
 add.addEventListener('click',() =>{
@@ -61,18 +61,18 @@ add.addEventListener('click',() =>{
     item.style.backgroundColor = bg
     item.style.borderRadius = `${b}px`
     item.style.position = pos
-    item.style.right = `${r}px`
     item.style.left = `${l}px`
-    item.style.bottom = `${bot}px`
     item.style.top = `${t}px`
+    item.style.right = `${r}px`
+    item.style.bottom = `${bot}px`
 
     width.value = ``
     height.value =``
     bgColor.value = ``
     br.value = ``
     position.value = ``
-    right.value = ``
     left.value = ``
-    bottom.value = ``
     top.value = ``
+    right.value = ``
+    bottom.value = ``
 })
